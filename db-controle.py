c7f967d26bbb377e0fcd517fe24e4836604918d3
EduLeal <eduardolealaruth@gmail.com>
2026-09-07 21:55:41 -0300

    feat: db de controle
    esse banco vai atuar como fila e meio de comunicação entre o crawler e o scraper

---
+++ db-controle.py
@@ -0,0 +1,25 @@
+# banco_controle.py
+import sqlite3
+
+def inicializar_banco():
+    conexao = sqlite3.connect('controle_fila.db')
+    cursor = conexao.cursor()
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS fila_reclamacoes (
+            hash_url TEXT PRIMARY KEY,
+            empresa TEXT NOT NULL,
+            url_completa TEXT NOT NULL,
+            status_reclamacao TEXT,
+            precisa_baixar INTEGER DEFAULT 1,
+            data_ultima_verificacao TEXT
+        )
+    ''')
+
+
+    conexao.commit()
+    conexao.close()
+    print("db no ar")
+
+if __name__ == '__main__':
+    inicializar_banco()
